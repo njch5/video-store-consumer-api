7350cf0f04a296d91471b52b48bab9d643ca929a
Nicky Choi <nickyjinchoi@gmail.com>
2019-12-16 15:04:02 -0800

    Added .env file with TheMovieDB API key

--- db/schema.rb
+++ db/schema.rb
@@ -10,40 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180618042754) do
+ActiveRecord::Schema.define(version: 2018_06_18_042754) do
 
-  create_table "customers", force: :cascade do |t|
-    t.string   "name"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "customers", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "registered_at"
-    t.string   "address"
-    t.string   "city"
-    t.string   "state"
-    t.string   "postal_code"
-    t.string   "phone"
-    t.float    "account_credit"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "postal_code"
+    t.string "phone"
+    t.float "account_credit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.text     "overview"
-    t.date     "release_date"
-    t.integer  "inventory"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.string   "image_url"
-    t.integer  "external_id"
+  create_table "movies", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.text "overview"
+    t.date "release_date"
+    t.integer "inventory"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_url"
+    t.integer "external_id"
   end
 
-  create_table "rentals", force: :cascade do |t|
-    t.integer  "customer_id"
-    t.integer  "movie_id"
-    t.date     "checkout_date"
-    t.date     "due_date"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.boolean  "returned"
+  create_table "rentals", id: :serial, force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "movie_id"
+    t.date "checkout_date"
+    t.date "due_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "returned"
     t.index ["customer_id"], name: "index_rentals_on_customer_id"
     t.index ["movie_id"], name: "index_rentals_on_movie_id"
   end
